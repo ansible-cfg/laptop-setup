dc32ca7d8c4f19d15e576fe9a1802c5f61a098ae
Bruno Paz <brunopaz@sapo.pt>
2018-01-27 11:28:24 +0000

    fetch latest version from github

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 
-REPO=" https://github.com/brpaz/laptop-setup.git"
+REPO="https://github.com/brpaz/laptop-setup.git"
 SETUP_DIR="$HOME/setup"
 CLONE_DIR="playbooks"
 
-echo -e "\033[33mPerforming initial system update\033[0m"
+echo -e "\033[33mPerforming initial system update and installing base dependencies\033[0m"
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
-sudo apt-get install -y python-pip python-setuptools git python-dev libxml2-dev libxslt-dev wget
-
-mkdir -p "$SETUP_DIR/downloads"
+sudo apt-get install -y python-pip python-apt python-setuptools git-core python-dev libxml2-dev libxslt-dev wget
 
 cd $SETUP_DIR
 if [ -d "$CLONE_DIR" ]; then
-  echo -e "\033[33mCloning playbooks\033[0m"
+  echo -e "\033[33mCloning playbook repository onto $CLONE_DIR\033[0m"
   git clone $REPO $CLONE_DIR
 fi
 
@@ -19,11 +17,11 @@ cd $CLONE_DIR
 
 echo -e "\033[33mStarting playbook ...\033[0m"
 sudo pip install -r requirements.txt
-ansible-playbook setup.yml -i HOSTS --ask-become-pass --module-path ./ansible_modules
 
-if [ !$? -eq 0 ]; then 
-  exit -1
-fi
+sudo chmod +x scripts/get_latest_release.sh 
+sudo mv scripts/get_latest_gh_release.sh /usr/local/bin
+
+ansible-playbook setup.yml -i HOSTS --ask-become-pass --module-path ./ansible_modules
 
 dropbox start -i
 
@@ -33,13 +31,18 @@ exec zsh
 
 echo -e "\033[33m<<EOF
 Machine provisioned with success.
-Next Steps:
-    - Install crashplan
-    - Install robo3t
-    - Sync vscode settings
-    - Sync PHPStorm Settings
-    - Sync Google Chrome and Firefox
-    - Login into applications
+A todo.txt file was created on your desktop indicating some manual steps to take after.
 EOF\033[0m"
+
+tee  ~/Desktop/setup.txt <<'EOF'
+Next Steps:
+  - Install crashplan
+  - Install robo3t
+  - Install gravit designer
+  - Sync vscode settings
+  - Sync PHPStorm Settings
+  - Sync Google Chrome and Firefox
+  - Login into applications
+EOF
 
 exit 0
