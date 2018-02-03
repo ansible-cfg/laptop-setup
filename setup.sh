9808ca68b5fedbc553bedd88f4bdf71990b9f79f
Bruno Paz <brunopaz@sapo.pt>
2018-02-03 16:12:08 +0000

    update tasks

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ REPO="https://github.com/brpaz/laptop-setup.git"
 SETUP_DIR="$HOME/setup"
 CLONE_DIR="$SETUP_DIR/playbooks"
 
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 echo "\033[33mPerforming initial system update and installing base dependencies\033[0m"
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 sudo apt-get install -y python-pip python-apt python-setuptools git-core python-dev libxml2-dev libxslt-dev wget
 
@@ -14,30 +14,31 @@ mkdir -p $SETUP_DIR
 cd $SETUP_DIR
 
 if [ ! -d "$CLONE_DIR" ]; then
-  echo "\033[33m---------------------------------------------------------------------[0m"
+  echo "\033[33m---------------------------------------------------------------------\033[0m"
   echo "\033[33mCloning playbook repository onto $CLONE_DIR\033[0m"
-  echo "\033[33m---------------------------------------------------------------------[0m"
+  echo "\033[33m---------------------------------------------------------------------\033[0m"
   git clone $REPO $CLONE_DIR
   cd $CLONE_DIR
 else
   cd $CLONE_DIR
-  echo "\033[33m---------------------------------------------------------------------[0m"
-  echo -e "\033[33mUpdating playbooks $CLONE_DIR\033[0m"
-  echo "\033[33m---------------------------------------------------------------------[0m"
+  echo "\033[33m---------------------------------------------------------------------\033[0m"
+  echo "\033[33mUpdating playbooks $CLONE_DIR\033[0m"
+  echo "\033[33m---------------------------------------------------------------------\033[0m"
   git reset HEAD --hard &&  git pull
 fi
 
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 echo  "\033[33mInstalling setup dependencies\033[0m"
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 sudo pip install -r requirements.txt
 
 sudo chmod +x scripts/get_latest_gh_release.sh 
+sudo chown $(whoami):$(whoami) scripts/get_latest_gh_release.sh 
 sudo cp scripts/get_latest_gh_release.sh /usr/local/bin
 
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 echo  "\033[33mRunning setup playbook\033[0m"
-echo "\033[33m---------------------------------------------------------------------[0m"
+echo "\033[33m---------------------------------------------------------------------\033[0m"
 ansible-playbook setup.yml -i HOSTS --ask-become-pass --module-path ./ansible_modules
 
 if [ ! $? -eq 0 ]; then
@@ -54,19 +55,23 @@ sudo tlp start
 exec zsh
 
 echo  "\033[33m<<EOF
-Machine provisioned with success.
-A todo.txt file was created on your desktop indicating some manual steps to take after.
-EOF\033[0m"
+The machine was provisioned with success. 
+Some steps still need to be executed manually:
 
-tee  ~/Desktop/setup.txt <<'EOF'
-Next Steps:
-  - Install crashplan
-  - Install robo3t
-  - Install gravit designer
-  - Sync vscode settings
-  - Sync PHPStorm Settings
-  - Sync Google Chrome and Firefox
-  - Login into applications
-EOF
+---------------------------------------------
+Manual Install Applications:
+ - Crashplan (https://support.code42.com/CrashPlan/4/Get_started/Install_the_Code42_app)
+ - Keyloard (https://protonail.com/)
+ - GravitDesigner (https://www.designer.io/#download)
+ - Robo3t (https://robomongo.org/)
+-----------------------------------------------
+
+---------------------------------------------
+Other
+ - Sync vscode and PHPStorm settings
+ - Login into applications
+ - Run hardcore icons fix
+-----------------------------------------------
+EOF"
 
 exit 0
